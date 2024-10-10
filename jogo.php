bdf283d5d4620107fa14a90973ca3e6bb0aa79e9
DIOGO <diogorpae@unesc.net>
2024-10-10 10:43:21 -0300

    att full perfil 1.0

--- jogo.php
+++ jogo.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GEOCONTEXT</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="jogo2.css"> <!-- Vincula o arquivo CSS externo -->
+    <link rel="stylesheet" href="jogo.css"> <!-- Vincula o arquivo CSS externo -->
 </head>
 <body>
     <div class="header">
